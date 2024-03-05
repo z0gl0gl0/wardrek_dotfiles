c0f5c73040b5db0cb16f0a37422691ad6ca38a2a
Wardrek <wardrek.hugo@gmail.com>
2024-03-05 11:13:29 +0100

    remove overide new line above/below

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -3,5 +3,8 @@ return {
     event = { "BufReadPost" },
     config = function ()
         vim.g.vim_markdown_conceal = 1
+        -- Disable overrides new line above/below
+        vim.keymap.set('n', '<Plug>', '<Plug>Markdown_NewLineAbove')
+        vim.keymap.set('n', '<Plug>', '<Plug>Markdown_NewLineBelow')
     end
 }
